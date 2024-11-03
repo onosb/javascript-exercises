8ea4cbc33fc7adde120f63e5a76f5dcdf57d6ca3
onosb <onoriodeerekaye@gmail.com>
2024-11-03 02:15:05 +0100

    Modify reverseString and reverseString.spec.js functions
    Modified reverseString function by making it to return any string in a reversed way using a for loop that index the string starting from its end (word.length) and dcrement until it reaches zero. And then removed .skip from test.skip in reverseString.spec.js.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversedWord = "";
+    for (i = word.length-1; i >= 0; i--) {
+        reversedWord += word[i];
+    }
+    return reversedWord;
 
 };
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
