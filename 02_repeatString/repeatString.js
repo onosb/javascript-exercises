3354acbe7658c9f52a570fa9af25b9cf8aaf26bc
onosb <onoriodeerekaye@gmail.com>
2024-11-03 01:25:09 +0100

    Modify repeatSring.js and repeatString.spec.js
     Modified repeastring function by adding two parameters- word and time, and using for loop to return word 3 times with the arguments- hey and 3. Next, removed all the .skip from all test function in repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let repeatedWord = "";
+    if (times < 0) {
+        return "ERROR";
+    }else {
+        for (i = 0; i < times; i++) {
+            repeatedWord += word; 
+        }
+    }
+    return repeatedWord;
 };
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
