39ef7d42d5a4c5a7e7f16163786f455840d0827f
onosb <onoriodeerekaye@gmail.com>
2024-12-19 00:20:35 +0100

    Create Two functions to convert temperature
    Create convertToCelsius and convertToFarenheit functions and return their value using parseint and toFixed to round to one decimal places.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenhiet) {
+  let celsius = (fahrenhiet - 32) * (5/9);
+  return parseFloat(celsius.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenhiet = celsius * (9/5) + 32;
+  return parseFloat(fahrenhiet.toFixed(1));
+
 };
 
 // Do not edit below this line
